4fe87e0a0f90001727775bbb65cc539a7c977831
Abdul Joarder <119489364+AJOARD18@users.noreply.github.com>
2023-01-30 09:33:49 +0000

    Update web-app.py

--- web-app.py
+++ web-app.py
@@ -13,7 +13,7 @@ streamlit.title('Associate Data Consultant - Abdul-Hafiz Joarder')
  
 
 streamlit.header('Profile')
-streamlit.text('I am an Associate Data Consultant with expertise in Excel and data cleaning.\nWith vast knowledge of programming languages SQL and Python including\nvarious Pandas libraries.\nSkilled in utilising data visualisation tools such as PowerBI and Tableau with\na strong ability to analyse and\nmanipulate data,\nas well as a proven track record of effectively communicating high-quality,\nkey insights to Stakeholders via written reportor presentation.')
+streamlit.text('I am an Associate Data Consultant with expertise in Excel and data cleaning.\nWith vast knowledge of programming languages SQL and Python including\nvarious Pandas libraries. Skilled in utilising data visualisation tools\nsuch as PowerBI and Tableau with\na strong ability to analyse and\nmanipulate data,\nas well as a proven track record of effectively communicating high-quality,\nkey insights to Stakeholders via written reportor presentation.')
 
 
 
